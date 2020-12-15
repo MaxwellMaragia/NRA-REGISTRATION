4a61fcb996dbfc9acc0c4e19c5d8eb83b551d661
Barnaby Kamau (Techno Brain Global FZE) <v-bakam@microsoft.com>
2020-12-15 12:25:58 +0300

    initial :tada:

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -20,6 +20,7 @@ import io.cucumber.junit.CucumberOptions;
 		tags = "@SUC:01-09",
 		dryRun = false,    //checks whether each feature has a mapped step definition
 		monochrome = true,// neat output after tc run
+
 		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","json:target/positive/cucumber.json", "pretty", "html:target/positive/cucumber.html"}
 		)
 
